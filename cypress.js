2988081ba00259dfbd1d448aaec943616ee5cba2
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2021-05-07 11:32:07 +0300

    Add example

---
+++ cypress.js
@@ -0,0 +1,18 @@
+const cypress = require('cypress');
+const config = require('./cypress.json');
+
+const updatedConfig = {
+  ...config,
+  reporterOptions: {
+    ...config.reporterOptions,
+    token: process.env.RP_TOKEN,
+  }
+};
+
+console.log('Updated config', updatedConfig);
+
+cypress.run(updatedConfig).then(
+  () => {
+    process.exit(0)
+  },
+);
