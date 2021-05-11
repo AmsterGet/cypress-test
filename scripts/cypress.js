d1ed80c6499544ecc08dafb297a8c57ec047872b
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2021-05-11 21:51:46 +0300

    Remove redundant code

--- scripts/cypress.js
+++ scripts/cypress.js
@@ -1,52 +1,18 @@
 const cypress = require('cypress');
-const fs = require('fs');
-const glob = require('glob');
-const { mergeLaunches } = require('@reportportal/agent-js-cypress/lib/mergeLaunches');
 const config = require('../cypress.json');
-
-const cypressConfigFile = 'cypress.json';
-
-const getLaunchTempFiles = () => {
-    return glob.sync('rplaunch-*.tmp');
-};
-
-const deleteTempFile = (filename) => {
-    fs.unlinkSync(filename);
-};
 
 cypress.run(config).then(
     (runInfo) => {
         console.log(runInfo);
-        console.log('----------------');
+        console.log('-----------------------------------------------------------');
         console.log(process.env);
-        fs.readFile(cypressConfigFile, 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
+        console.log('-----------------------------------------------------------');
+        console.log(process);
+        process.exit(0);
 
-            const config = JSON.parse(data);
-
-            if (config.reporterOptions.isLaunchMergeRequired) {
-                mergeLaunches(config.reporterOptions)
-                    .then(() => {
-                        const files = getLaunchTempFiles();
-                        files.forEach(deleteTempFile);
-                        console.log('Launches successfully merged!');
-                        process.exit(0);
-                    })
-                    .catch((err) => {
-                        console.error(error);
-                        process.exit(1);
-                    });
-            } else {
-                process.exit(0);
-            }
-        });
     },
     (error) => {
         console.error(error);
-        const files = getLaunchTempFiles();
-        files.forEach(deleteTempFile);
         process.exit(1);
     },
 );
